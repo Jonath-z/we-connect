7aac8d4b428dd800840a4da5b9360d5607bd52f9
Jonath-z <jonathanzihindula95@gmail.com>
2022-08-08 18:40:21 +0200

    Message context initialized

--- lib/contexts/MessageContext.tsx
+++ lib/contexts/MessageContext.tsx
@@ -1,4 +1,17 @@
-import React from "react";
+import { TMessage } from "lib/types";
+import React, { createContext, useContext } from "react";
+
+interface IMessageContext {
+  sendMessage: (message: TMessage) => TMessage | null;
+}
+
+const defaultContext: IMessageContext = {
+  sendMessage: () => null,
+};
+
+const MessageContext = createContext(defaultContext);
+
+export const useMessage = () => useContext(MessageContext);
 
 const MessageProvider = () => {
   return <div>MessageProvider</div>;
